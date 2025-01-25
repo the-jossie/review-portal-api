cab7838ec91319f53daf1126ab6d9440c6ebfa91
Dev Jo <idowuayanfeoluwa@gmail.com>
2025-01-25 19:23:32 +0000

    feat: Login Dto

---
+++ Dtos/LoginDto.cs
@@ -0,0 +1,6 @@
+namespace Restaurant_Review_Api.Dtos{
+    public partial class LoginDto{
+         public string Email {get; set;} = "";
+         public string Password {get; set;} = "";
+    }
+}
