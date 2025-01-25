19e7d138f0bb18e6dbe8938b55a09de9e6d752ed
Dev Jo <idowuayanfeoluwa@gmail.com>
2025-01-25 20:59:32 +0000

    feat: added auth repository

---
+++ Repositories/AuthRepository.cs
@@ -0,0 +1,28 @@
+using Restaurant_Review_Api.Data;
+using Restaurant_Review_Api.Models;
+
+namespace Restaurant_Review_Api.Repositories
+{
+    public interface IAuthRepository : IRepositoryBase
+    {
+        public IEnumerable<Auth> GetAllUsers();
+    }
+
+    public class AuthRepository : RepositoryBase
+    {
+        DataContext _entityFramework;
+
+        public AuthRepository(IConfiguration config) : base(config)
+        {
+            _entityFramework = new DataContext(config);
+        }
+
+        public IEnumerable<Auth> GetAllUsers()
+        {
+            // IEnumerable<Auth> users = _entityFramework.AuthUsers.ToList<Auth>();
+            IEnumerable<Auth> users = [.. _entityFramework.AuthUsers];
+
+            return users;
+        }
+    }
+}
