296446a349a35ab2a3cbae8584064fb5d01a25fe
Dev Jo <idowuayanfeoluwa@gmail.com>
2025-01-25 19:23:24 +0000

    feat: Signup Dto

---
+++ Dtos/SignupDto.cs
@@ -0,0 +1,10 @@
+namespace Restaurant_Review_Api.Dtos
+{
+    public partial class SignupDto
+    {
+        public string Email { get; set; } = "";
+        public string UserName { get; set; } = "";
+        public string Password { get; set; } = "";
+        public string PasswordConfirmation { get; set; } = "";
+    }
+}
