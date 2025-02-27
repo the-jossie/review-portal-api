8ae6503ab9fbfef4fca58d00f749b60c60ba00a2
Dev Jo <idowuayanfeoluwa@gmail.com>
2025-02-27 02:22:48 +0000

    fix: auth model

--- Models/Auth.cs
+++ Models/Auth.cs
@@ -1,9 +1,11 @@
-namespace Restaurant_Review_Api.Models
+namespace Ca_Bank_Api.Models
 {
     public partial class Auth
     {
-        public string Email {get; set;} = "";
+        public string Email { get; set; } = "";
         public byte[] PasswordHash { get; set; } = [0];
         public byte[] PasswordSalt { get; set; } = [0];
+        public string? OTP { get; set; }
+        public DateTime? OTPExpiry { get; set; }
     }
 }
