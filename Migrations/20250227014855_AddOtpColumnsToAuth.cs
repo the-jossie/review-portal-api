79ec15d7acd8275f4bf6d37c638238103209c317
Dev Jo <idowuayanfeoluwa@gmail.com>
2025-02-27 02:24:30 +0000

    feat: created migrations

---
+++ Migrations/20250227014855_AddOtpColumnsToAuth.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ca_bank_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOtpColumnsToAuth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "CaBankSchema");
+
+            migrationBuilder.CreateTable(
+                name: "Auth",
+                schema: "CaBankSchema",
+                columns: table => new
+                {
+                    Email = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    PasswordHash = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    PasswordSalt = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    OTP = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OTPExpiry = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Auth", x => x.Email);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                schema: "CaBankSchema",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserRole = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Auth",
+                schema: "CaBankSchema");
+
+            migrationBuilder.DropTable(
+                name: "Users",
+                schema: "CaBankSchema");
+        }
+    }
+}
